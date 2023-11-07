472a28c42a661a0a4d0fd8a20fa31b69cae82f06
GiselleLR Ramirez <giselleramirez0929@gmail.com>
2023-11-07 19:01:25 +0000

    upgrade

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+AFRAME.registerComponent("doorbell",{
 
+    schema: {
+        sound:{type:"string", default:"path"},
+        duaration: {type:"number", default: 5}
+    },
+
+    init: function() {
+        this.el.addEventListener('click', function(onj))
+    }
+
+
+
+
+})
